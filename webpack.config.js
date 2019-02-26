4e6275f11b51170fe4417a42ef48e06e90b1e878
Wojciech Jeziorski <wojciech.wujot@gmail.com>
2019-02-26 21:59:40 +0100

    Add config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,38 @@
 const path = require("path");
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "app.bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: "babel-loader"
-      },
-      {
-        test: /\.css$/,
-        use: [
-          { loader: "style-loader" },
-          {
-            loader: "css-loader",
-            options: {
-              modules: true
+//webpack.config.js
+module.exports = env => {
+  const environment = env || "production";
+
+  return {
+    optimization: {
+      minimize: false
+    },
+    mode: environment,
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "app." + environment + ".bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: "babel-loader"
+        },
+        {
+          test: /\.css$/,
+          use: [
+            { loader: "style-loader" },
+            {
+              loader: "css-loader",
+              options: {
+                modules: true
+              }
             }
-          }
-        ]
-      }
-    ]
-  }
+          ]
+        }
+      ]
+    }
+  };
 };
